be419957175220e7d05f5b0f2d7681cf38ba7d04
Gabriel-Emannuel <gabriel.emannuel.gama.andrade@gmail.com>
2024-03-30 20:04:48 -0300

    implementando melhorias

--- src/Tetris/JOGO/Main.hs
+++ src/Tetris/JOGO/Main.hs
@@ -39,19 +39,32 @@ renderizacao estado = return $ pictures [
     caixaPontuacao = renderizaTexto "Pontuacao" (show (pontuacao estado)) (150, -150, 0.2, 0.2)
     caixaTempo = renderizaTexto "Tempo" (show (tempo estado)) (150, -300, 0.2, 0.2)
     caixaProximaPeca = renderizaProximaPeca (head (formatosPeca (proximaPeca estado))) (150, 10) (180, -60) "Proxima"
-    caixaComandoA = renderizaTexto "A - mover a peca para esquerda" "" (-400, 250, 0.1, 0.1)
-    caixaComandoD = renderizaTexto "D - mover a peca para direita" "" (-400, 200, 0.1, 0.1)
-    caixaComandoS = renderizaTexto "S - mover a peca para Baixo" "" (-400, 150, 0.1, 0.1)
-    caixaComandoR = renderizaTexto "R - reniciar Jogo" "" (-400, 100, 0.1, 0.1)
-    caixaComandoK = renderizaTexto "K - Rotacionar anti-horario" "" (-400, 50, 0.1, 0.1)
-    caixaComandoL = renderizaTexto "L - Rotacionar horario" "" (-400, 1, 0.1, 0.1)
-    caixaComandoX = renderizaTexto "X - Jogar a peca pra baixo" "" (-400, -50, 0.1, 0.1)
+    caixaComandoA = renderizaTexto "seta pra esquerda mover pra esquerda" "" (-420, 250, 0.1, 0.1)
+    caixaComandoD = renderizaTexto "seta pra direita mover pra direita" "" (-420, 200, 0.1, 0.1)
+    caixaComandoS = renderizaTexto "seta pra baixo mover pra Baixo" "" (-420, 150, 0.1, 0.1)
+    caixaComandoR = renderizaTexto "R - reniciar Jogo" "" (-420, 100, 0.1, 0.1)
+    caixaComandoK = renderizaTexto "Z - Rotacionar anti-horario" "" (-420, 50, 0.1, 0.1)
+    caixaComandoL = renderizaTexto "X - Rotacionar horario" "" (-420, 1, 0.1, 0.1)
+    caixaComandoX = renderizaTexto "espaco - Jogar a peca pra baixo" "" (-420, -50, 0.1, 0.1)
     caixaPerdeu = if jogoAcabou estado then renderizaTexto "Voce" "Perdeu" (150, 100, 0.2, 0.2) else renderizaTexto "" "" (150, -350, 0.2, 0.2)
 
 inputTeclado :: Event -> Estado -> IO Estado
 inputTeclado (EventKey (Char t) Down _ _ ) estado
   | tecla /= 'r' &&  jogoAcabou estado = return estado
-  | tecla == 'a' = return $ if verificaShiftEsquerda (grid estado)
+  | tecla == 'z' = return $
+      if verificaRotacao (grid estado) (atualPeca estado) True
+      then rotacionaPeca estado True
+      else estado
+  | tecla == 'x' = return $
+      if verificaRotacao (grid estado) (atualPeca estado) False
+      then rotacionaPeca estado False
+      else estado
+  | tecla == 'r' = return geraEstadoInicial
+  | otherwise = return estado
+  where
+    tecla = toLower t
+inputTeclado (EventKey (SpecialKey tecla) Down _ _) estado 
+  | tecla == KeyLeft = return $ if verificaShiftEsquerda (grid estado)
       then estado {
         grid = shiftEsquerda (grid estado),
         atualPeca = (atualPeca estado) {
@@ -59,7 +72,7 @@ inputTeclado (EventKey (Char t) Down _ _ ) estado
         }
         }
     else estado
-  | tecla == 'd' = return $ if verificaShiftDireita (grid estado)
+  | tecla == KeyRight = return $ if verificaShiftDireita (grid estado)
       then estado {
         grid = shiftDireita (grid estado),
         atualPeca = (atualPeca estado) {
@@ -67,7 +80,7 @@ inputTeclado (EventKey (Char t) Down _ _ ) estado
         }
         }
     else estado
-  | tecla == 's' = return $ 
+  | tecla == KeyDown = return $ 
       if verificaShiftBaixo (grid estado)
       then estado {
         grid = shiftBaixo (grid estado),
@@ -77,29 +90,19 @@ inputTeclado (EventKey (Char t) Down _ _ ) estado
         }
         }
       else estado
-  | tecla == 'x' = descerCompleto estado
-  | tecla == 'k' = return $
-      if verificaRotacao (grid estado) (atualPeca estado) True
-      then rotacionaPeca estado True
-      else estado
-  | tecla == 'l' = return $
-      if verificaRotacao (grid estado) (atualPeca estado) False
-      then rotacionaPeca estado False
-      else estado
-  | tecla == 'r' = return geraEstadoInicial
-  | otherwise = return estado
-  where
-    tecla = toLower t
-    ((x1, y1), (x2,y2)) = coordenadas (atualPeca estado)
-    descerCompleto estado = if verificaShiftBaixo (grid estado) 
-      then descerCompleto estado {
-        grid = shiftBaixo (grid estado),
-        pontuacao = pontuacao estado + 1,
-        atualPeca = (atualPeca estado) {
-        coordenadas = ((x1, y1-1), (x2,y2-1))
-        }
-      } 
-    else atualizaTempo 1 estado
+  | tecla == KeySpace = descerCompleto estado
+    where 
+       ((x1, y1), (x2,y2)) = coordenadas (atualPeca estado)
+       descerCompleto estado = 
+        if verificaShiftBaixo (grid estado) 
+        then descerCompleto estado {
+          grid = shiftBaixo (grid estado),
+          pontuacao = pontuacao estado + 5 * nivel estado,
+          atualPeca = (atualPeca estado) {
+          coordenadas = ((x1, y1-1), (x2,y2-1))
+          }
+        } 
+        else atualizaTempo 1 estado
 
 inputTeclado _ estado = return estado
 
